a7ad462c0232ab967209442d8968b89b02b5d173
svetlana_kuznetsova1@epam.com <svetlana_kuznetsova1@epam.com>
2020-10-19 23:29:00 +0300

    feat: logging & error handling

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
+const { INTERNAL_SERVER_ERROR, getStatusText } = require('http-status-codes');
+
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
+const logger = require('./common/logger');
 
 const app = express();
+
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
@@ -21,7 +25,32 @@ app.use('/', (req, res, next) => {
   next();
 });
 
+app.use('/', (req, res, next) => {
+  logger.info(
+    `method: ${req.method}, url: ${req.url}, query params: ${JSON.stringify(
+      req.query
+    )}, body: ${JSON.stringify(req.body)}`
+  );
+  next();
+});
+
+process.on('uncaughtException', err => {
+  console.error(`Captured error: ${err.message}`);
+  // eslint-disable-next-line no-process-exit
+  process.exit(1);
+});
+
+process.on('unhandledRejection', reason => {
+  console.error(`Unhandled rejection detected: ${reason.message}`);
+});
+
 app.use('/users', userRouter);
 app.use('/boards', boardRouter, taskRouter);
+
+app.use((err, req, res, next) => {
+  logger.error(err.message);
+  res.status(INTERNAL_SERVER_ERROR).send(getStatusText(INTERNAL_SERVER_ERROR));
+  next(err);
+});
 
 module.exports = app;
